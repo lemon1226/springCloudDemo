2826d3ddc9b446ac84fbf3c2a48cb9fe712624c8
zhangmeng <32800438@qq.com>
2019-07-11 11:31:40 +0800

    增加 security 权限模块

---
+++ service-security/src/main/java/com/lemon/servicesecurity/ServiceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lemon.servicesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSecurityApplication.class, args);
+    }
+
+}
