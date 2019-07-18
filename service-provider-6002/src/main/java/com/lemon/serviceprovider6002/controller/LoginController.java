e45d193341240a8ef24a685f11b2a976d437042f
zhangmeng <32800438@qq.com>
2019-07-18 17:33:08 +0800

    修改些内容

--- service-provider-6002/src/main/java/com/lemon/serviceprovider6002/controller/LoginController.java
+++ service-provider-6002/src/main/java/com/lemon/serviceprovider6002/controller/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 /**
  * description:
  *
@@ -20,7 +22,7 @@ public class LoginController {
     public LoginResult loginIn(@RequestBody LoginVo loginVo){
         LoginResult result = new LoginResult();
         result.setMsg("登录成功，来自6002，内容：" + JSON.toJSONString(loginVo));
-        result.setLoginTime(loginVo.getLoginTime());
+        result.setLoginTime(new Date());
         return result;
     }
 }
--- servicebase/src/main/java/com/lemon/utils/ResultUtil.java
+++ servicebase/src/main/java/com/lemon/utils/ResultUtil.java
@@ -20,29 +20,44 @@ public class ResultUtil{
 
     public static Result success(Object re, String msg){
         Result result = new Result();
-        result.setCode(1);
+        result.setCode(0);
         result.setMsg(msg);
         result.setRe(re);
         return result;
     }
 
-    public static Result fail(){
-        return fail(null);
+    public static Result busFail(){
+        return busFail(-1);
     }
 
-    public static Result fail(Object re){
-        return fail(re, "失败");
+    public static Result busFail(Object re){
+        return busFail(-1, re, "失败");
+    }
+
+    public static Result busFail(Integer code){
+        return busFail(code, null, "失败");
+    }
+
+    public static Result busFail(Integer code, String msg){
+        return busFail(code, null, msg);
+    }
+
+    public static Result busFail(String msg){
+        return busFail(-1, null, msg);
+    }
+
+    public static Result busFail(Integer code, Object re, String msg){
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setRe(re);
+        return result;
     }
 
     public static Result fail(String msg){
-        return fail(null, "msg");
-    }
-
-    public static Result fail(Object re, String msg){
         Result result = new Result();
-        result.setCode(0);
+        result.setCode(-2);
         result.setMsg(msg);
-        result.setRe(re);
         return result;
     }
 }
